d2668a83eba2e08f1525861dcc38246deea3d506
nsairekha <rekhans321@gmail.com>
2025-09-13 19:52:18 +0530

    modified nav bar

--- my-app/src/app/components/chat/navbar/navbar.tsx
+++ my-app/src/app/components/chat/navbar/navbar.tsx
@@ -1,10 +1,33 @@
 import React from 'react'
+import './navbar.css'
+import { FiShare } from "react-icons/fi";
+import { IoSparklesSharp } from "react-icons/io5";
+import { IoSettingsOutline } from "react-icons/io5";
 
 const navbar = () => {
   return (
     <div className="ChatNavbar">
       <div className="ChatNavbar-in">
-        <h1>This is Chat Navbar</h1>
+        <div className="ChatNavbar-in-one">
+          <div className="ChatNavbar-in-one-a">
+          <h1>Orbita GPT</h1>
+          </div>
+          <div className="ChatNavbar-in-one-b">
+            <button>plus</button>
+          </div>
+        </div>
+        <div className="ChatNavbar-in-two">
+          <div className="ChatNavbar-in-two-a">
+            <button>Configuration <IoSettingsOutline /></button>
+            
+          </div>
+          <div className="ChatNavbar-in-two-b">
+            <button>Share <FiShare /></button>
+          </div>
+          <div className="ChatNavbar-in-two-c">
+            <button>New Chat <IoSparklesSharp /></button>
+          </div>
+        </div>
       </div>
     </div>
   )
