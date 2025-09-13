70faff7972033e7986aad4edd4439c57387ca16a
saitadikonda99 <saitadikonda.tech@gmail.com>
2025-09-13 16:58:26 +0530

    feat: initialize Next.js project with default setup

---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,11 @@
+import Image from "next/image";
+
+export default function Home() {
+  return (
+      <div className="HomeComponent">
+        <div className="HomeComponent-in">
+          <h1>Initial Setup</h1> 
+        </div>
+      </div>
+  );
+}
